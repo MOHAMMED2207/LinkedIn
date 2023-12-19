77a0b55aa07e3acc37eb1d0d7dee2479148592f4
MOHAMMED <120588577+MOHAMMED2207@users.noreply.github.com>
2023-12-19 19:02:03 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,20 +8,20 @@ import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBACE_APIKEY,
-  // authDomain: process.env.REACT_APP_FIREBACE_authDomain,
-  // projectId: process.env.REACT_APP_FIREBACE_projectId,
-  // storageBucket: process.env.REACT_APP_FIREBACE_storageBucket,
-  // messagingSenderId: process.env.REACT_APP_FIREBACE_messagingSenderId,
-  // appId: process.env.REACT_APP_FIREBACE_appId,
-  // measurementId: process.env.REACT_APP_FIREBACE_measurementId,
-  apiKey: "AIzaSyD5gUeq8X5IqZLFwDnoRupnHSZTWTRgXqY",
-  authDomain: "linkedinclone-e2706.firebaseapp.com",
-  projectId: "linkedinclone-e2706",
-  storageBucket: "linkedinclone-e2706.appspot.com",
-  messagingSenderId: "704046675382",
-  appId: "1:704046675382:web:e151e9011a22dfa1c93d17",
-  measurementId: "G-C28W601X88",
+  apiKey: process.env.REACT_APP_FIREBACE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBACE_authDomain,
+  projectId: process.env.REACT_APP_FIREBACE_projectId,
+  storageBucket: process.env.REACT_APP_FIREBACE_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIREBACE_messagingSenderId,
+  appId: process.env.REACT_APP_FIREBACE_appId,
+  measurementId: process.env.REACT_APP_FIREBACE_measurementId,
+  // apiKey: "AIzaSyD5gUeq8X5IqZLFwDnoRupnHSZTWTRgXqY",
+  // authDomain: "linkedinclone-e2706.firebaseapp.com",
+  // projectId: "linkedinclone-e2706",
+  // storageBucket: "linkedinclone-e2706.appspot.com",
+  // messagingSenderId: "704046675382",
+  // appId: "1:704046675382:web:e151e9011a22dfa1c93d17",
+  // measurementId: "G-C28W601X88",
 };
 
 const App = initializeApp(firebaseConfig);
